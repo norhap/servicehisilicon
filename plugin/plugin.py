d5366a144940835fd42b999be2d14ef0ae77737d
Captain <captain.onboard@web.de>
2020-06-02 15:07:03 +0200

    [2to3] add PyModule_Create for Python3

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -1,7 +1,8 @@
+from __future__ import absolute_import
 from Plugins.Plugin import PluginDescriptor
 
 def autostart(reason, **kwargs):
-	import servicehisilicon
+	from . import servicehisilicon
 
 def Plugins(**kwargs):
 	return [
