252f7735e6628816c726852643c1e765945cf650
Twol <tonywhalley@t-online.de>
2022-01-25 20:12:41 +0100

    pythonmodule clean space/tabs and align

--- servicehisilicon/pythonmodule.cpp
+++ servicehisilicon/pythonmodule.cpp
@@ -6,21 +6,21 @@ static PyMethodDef servicehisiliconMethods[] =
 };
 
 #if PY_MAJOR_VERSION >= 3
-	static struct PyModuleDef moduledef = {
-		PyModuleDef_HEAD_INIT,
-		"servicehisilicon",					/* m_name */
-		"Module for servicehisilicon",		/* m_doc */
-		-1,									/* m_size */
-		servicehisiliconMethods,			/* m_methods */
-		NULL,								/* m_reload */
-		NULL,								/* m_traverse */
-		NULL,								/* m_clear */
-		NULL,								/* m_free */
-	};
+static struct PyModuleDef moduledef = {
+	PyModuleDef_HEAD_INIT,
+	"servicehisilicon",		/* m_name */
+	"servicehisilicon",		/* m_doc */
+	-1,				/* m_size */
+	servicehisiliconMethods,	/* m_methods */
+	NULL,				/* m_reload */
+	NULL,				/* m_traverse */
+	NULL,				/* m_clear */
+	NULL,				/* m_free */
+};
 
 PyMODINIT_FUNC PyInit_servicehisilicon(void)
 {
-    return PyModule_Create(&moduledef);
+	return PyModule_Create(&moduledef);
 }
 #else
 
